c191dd88f9694662f6d7c23ebeaf33a2290f8ce8
ManishDeorari <manishdeorari377@example.com>
2025-07-02 20:50:44 +0530

    Fixed unescaped quote issue for production

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: "https://alumni-frontend.vercel.app", // or "*"
+  credentials: true,
+}));
 
 // Connect to MongoDB
 connectDB();
