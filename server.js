8ed068619b2c965aa0ab09cd0bbbac0c3d74e788
ManishDeorari <manishdeorari377@example.com>
2025-07-12 11:40:09 +0530

    Fixed unescaped quote issue for production

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"], // ✅ This is required
