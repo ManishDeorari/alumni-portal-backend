9a62b1335222e0a7a6ad34461251497dfd3dedd5
ManishDeorari <manishdeorari377@example.com>
2025-07-18 20:34:08 +0530

    Remove undefined commentPost route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,40 +4,39 @@ const { verifyToken } = require("../middleware/authMiddleware");
 
 const {
   getPosts,
-  createPost,
-  likePost,
-  reactToPost,
-  commentPost,
-  replyToComment,
-  deleteComment,
-  editPost,
-  deletePost,
-  editComment,
-} = require("../api/posts/postController");
+  //createPost,
+  //likePost,
+  //reactToPost,
+  //commentPost,
+  //replyToComment,
+  //deleteComment,
+  //editPost,
+  //deletePost,
+  //editComment,
+} = require("../src/api/posts/postController");
 
 console.log("Imported functions from postController:", {
   getPosts,
-  createPost,
-  likePost,
-  reactToPost,
-  commentPost,
-  replyToComment,
-  deleteComment,
-  editPost,
-  deletePost,
-  editComment,
+  //createPost,
+  //likePost,
+  //reactToPost,
+  //commentPost,
+  //replyToComment,
+  //deleteComment,
+  //editPost,
+  //deletePost,
+  //editComment,
 });
 
-
 router.get("/", getPosts);
-router.post("/", verifyToken, createPost);
-router.patch("/:id/like", verifyToken, likePost);
-router.patch("/:id/react", verifyToken, reactToPost);
-router.post("/:id/comment", verifyToken, commentPost);
-router.post("/:postId/comment/:commentId/reply", verifyToken, replyToComment);
-router.delete("/:postId/comment/:commentId", verifyToken, deleteComment);
-router.patch("/:id", verifyToken, editPost);
-router.delete("/:id", verifyToken, deletePost);
-router.put("/:postId/comments/:commentId", verifyToken, editComment);
+//router.post("/", verifyToken, createPost);
+//router.patch("/:id/like", verifyToken, likePost);
+//router.patch("/:id/react", verifyToken, reactToPost);
+//router.post("/:id/comment", verifyToken, commentPost);
+//router.post("/:postId/comment/:commentId/reply", verifyToken, replyToComment);
+//router.delete("/:postId/comment/:commentId", verifyToken, deleteComment);
+//router.patch("/:id", verifyToken, editPost);
+//router.delete("/:id", verifyToken, deletePost);
+//router.put("/:postId/comments/:commentId", verifyToken, editComment);
 
 module.exports = router;
