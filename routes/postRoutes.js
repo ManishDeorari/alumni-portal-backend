6615333162b89b69363a582a6e295d1aa6d5dc79
ManishDeorari <manishdeorari377@example.com>
2025-07-18 20:41:20 +0530

    Remove undefined commentPost route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,38 +5,25 @@ const auth = require("../middleware/authMiddleware");
 const {
   getPosts,
   createPost,
-  //likePost,
-  //reactToPost,
-  //commentPost,
-  //replyToComment,
-  //deleteComment,
-  //editPost,
-  //deletePost,
-  //editComment,
+  likePost,
+  reactToPost,
+  commentPost,
+  replyToComment,
+  deleteComment,
+  editPost,
+  deletePost,
+  editComment,
 } = require("../src/api/posts/postController");
-
-console.log("Imported functions from postController:", {
-  getPosts,
-  createPost,
-  //likePost,
-  //reactToPost,
-  //commentPost,
-  //replyToComment,
-  //deleteComment,
-  //editPost,
-  //deletePost,
-  //editComment,
-});
 
 router.get("/", getPosts);
 router.post("/", auth, createPost);
-//router.patch("/:id/like", verifyToken, likePost);
-//router.patch("/:id/react", verifyToken, reactToPost);
-//router.post("/:id/comment", verifyToken, commentPost);
-//router.post("/:postId/comment/:commentId/reply", verifyToken, replyToComment);
-//router.delete("/:postId/comment/:commentId", verifyToken, deleteComment);
-//router.patch("/:id", verifyToken, editPost);
-//router.delete("/:id", verifyToken, deletePost);
-//router.put("/:postId/comments/:commentId", verifyToken, editComment);
+router.patch("/:id/like", auth, likePost);
+router.patch("/:id/react", auth, reactToPost);
+router.post("/:id/comment", auth, commentPost);
+router.post("/:postId/comment/:commentId/reply", auth, replyToComment);
+router.delete("/:postId/comment/:commentId", auth, deleteComment);
+router.patch("/:id", auth, editPost);
+router.delete("/:id", auth, deletePost);
+router.put("/:postId/comments/:commentId", auth, editComment);
 
 module.exports = router;
