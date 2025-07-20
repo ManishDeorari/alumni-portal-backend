577a18db87ce511bcc1bc29edc8a280d88122cbf
ManishDeorari <manishdeorari377@example.com>
2025-07-20 21:11:17 +0530

    Fixed unescaped quote issue for production

--- src/api/posts/post/reactToPost.js
+++ src/api/posts/post/reactToPost.js
@@ -1,7 +1,5 @@
 const Post = require("../../../../models/Post");
 
-const Post = require("../../../../models/Post");
-
 module.exports = async (req, res) => {
   try {
     const { id } = req.params;
