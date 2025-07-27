9f8fe6351ed1dcf6370a54af63133cd80e30df2c
ManishDeorari <manishdeorari377@example.com>
2025-07-27 23:20:23 +0530

    Fixed unescaped quote issue for production

--- src/api/posts/comment/deleteComment.js
+++ src/api/posts/comment/deleteComment.js
@@ -2,11 +2,14 @@ const Post = require("../../../../models/Post");
 
 const deleteComment = async (req, res) => {
   try {
+    console.log("🔐 req.user =", req.user);
     const post = await Post.findById(req.params.postId);
     if (!post) return res.status(404).json({ message: "Post not found" });
 
     const comment = post.comments.id(req.params.commentId);
     if (!comment) return res.status(404).json({ message: "Comment not found" });
+
+    console.log("🧾 comment.user =", comment.user);
 
     if (comment.user.toString() !== req.user._id.toString()) {
       return res.status(403).json({ message: "Unauthorized" });
