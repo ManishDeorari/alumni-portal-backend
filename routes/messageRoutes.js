77b4ec9ee31c8c0772eae3da6cc0864b041f8526
ManishDeorari <manishdeorari377@gmail.com>
2026-01-30 08:40:11 +0530

    Fixed unescaped quote issue for production

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const checkAuth = require("../middleware/checkAuth");
+const checkAuth = require("../middleware/authMiddleware");
 const Message = require("../models/Message");
 const User = require("../models/User");
 
